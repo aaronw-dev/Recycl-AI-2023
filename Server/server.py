5cd5c4b83dc269e70c4cca623210b7a1a9a13442
AW_Dev <riptidegamesceo@gmail.com>
2023-06-25 14:38:00 -0700

    idk random live share stuffs and camera added and backend changed and albert broke the code
    Co-authored-by: IngeniousCoders <IngeniousCoders@users.noreply.github.com>

--- Server/server.py
+++ Server/server.py
@@ -1,19 +1,56 @@
-from flask import Flask, render_template
+from ultralytics import YOLO
+from flask import Flask, render_template, request, Response
+from waitress import serve
+from PIL import Image
+import json
+#import torch
 # Import Flask Class, and render_template
 app = Flask(__name__) # Create an Instance
 
 @app.route('/')
 def homepage():
   return render_template('index.html')
+
 @app.route('/inference')
 def inferencepage():
   return render_template('inference.html')
   
 @app.errorhandler(500)
 def fivehundrederror(error):
-  return render_template('error.html', errorcode = error)
+  return render_template('error_500.html')
   
 @app.errorhandler(404)
 def invalid_route(error):
-  return render_template('error.html')
-app.run(host='0.0.0.0', port=5000, debug=False)
+  return render_template('error_404.html')
+
+
+# Albert's code'
+
+@app.route("/detect", methods=["POST"])
+def detect():
+    buf = request.files["image_file"]
+    boxes = detect_objects_on_image(Image.open(buf.stream))
+    return Response(
+      json.dumps(boxes),  
+      mimetype='application/json'
+    )
+
+def detect_objects_on_image(buf):
+    model = YOLO("best.pt")
+    results = model.predict(buf)
+    result = results[0]
+    output = []
+    for box in result.boxes:
+        x1, y1, x2, y2 = [
+          round(x) for x in box.xyxy[0].tolist()
+        ]
+        class_id = box.cls[0].item()
+        prob = round(box.conf[0].item(), 2)
+        output.append([
+          x1, y1, x2, y2, result.names[class_id], prob
+        ])
+    return output
+
+
+#Albet's code
+app.run(host='0.0.0.0', port=3000, debug=False)
--- Server/static/inference.js
+++ Server/static/inference.js
@@ -1,3 +1,11 @@
+let imageToDetect = null;
+
+const videoContainer = document.getElementById('videoContainer');
+const imageContainer = document.getElementById('imageContainer');
+const capturedImage = document.getElementById('capturedImage');
+const captureButton = document.getElementById('capture');
+const returnButton = document.getElementById('returnButton');
+
 const detectedobject = document.getElementById("detectedobject")
 const itemconfidence = document.getElementById("item-confidence")
 const itemtext = document.getElementById("item-text")
@@ -7,3 +15,47 @@ detectedobject.innerHTML = "something random"
 itemconfidence.innerHTML = "3%"
 itemtext.innerHTML = "thing in front of the camera"
 itemdetails.innerHTML = "It goes in the landfill but if I actually remember to finish writing this then it might not."
+const maxVideoWidth = 640; // Maximum allowed video width
+const maxVideoHeight = 640; // Maximum allowed video height
+
+// Get access to the user's webcam with limited resolution
+navigator.mediaDevices.getUserMedia({ 
+  video: {
+    width: { ideal: maxVideoWidth },
+    height: { ideal: maxVideoHeight }
+  }
+})
+  .then((stream) => {
+    const videoElement = document.getElementById('video');
+    videoElement.srcObject = stream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  })
+  .catch((error) => {
+    console.error('Error accessing webcam:', error);
+  });
+
+captureButton.addEventListener('click', () => {
+  const videoElement = document.getElementById('video');
+  videoElement.hidden = true;
+  const canvas = document.createElement('canvas');
+  canvas.width = videoElement.videoWidth;
+  canvas.height = videoElement.videoHeight;
+  const context = canvas.getContext('2d');
+  context.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
+
+  // Save the captured image to the "imageToDetect" variable
+  imageToDetect = canvas.toDataURL('image/png');
+
+  // Display the captured image
+  capturedImage.src = imageToDetect;
+});
+
+returnButton.addEventListener('click', () => {
+  const videoElement = document.getElementById('video');
+  videoElement.hidden = false;
+  // Clear the captured image and return to the live webcam feed
+  imageToDetect = null;
+  capturedImage.src = '';
+});
