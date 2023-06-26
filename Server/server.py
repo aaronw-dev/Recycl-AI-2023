66930d52a314d423c7fa7011416814914ec67163
AW_Dev <riptidegamesceo@gmail.com>
2023-06-25 18:29:07 -0700

    THE AI FKIN WORKS WE DID IT BOIS

--- Server/server.py
+++ Server/server.py
@@ -1,12 +1,25 @@
+from flask import Flask, render_template, request
+import base64
+print("Importing AI Packages...")
+print("Importing YOLO...")
 from ultralytics import YOLO
-from flask import Flask, render_template, request, Response
-from waitress import serve
+print("YOLO imported. Importing numpy...")
+import numpy as np
+print("numpy imported. Importing PIL...")
 from PIL import Image
-import json
-#import torch
-# Import Flask Class, and render_template
-app = Flask(__name__) # Create an Instance
+print("PIL imported.")
 
+print("Loading model...")
+model = YOLO("Server\\best.pt")
+print("Model loaded.")
+
+app = Flask(__name__) # Create an Instance
+names = {
+  0: 'Crinkly Plastic', 
+  1: 'Deposit_Recycle', 
+  2: 'Green_bin', 
+  3: 'Metallic Wrapping', 
+  4: 'Recycle'}
 @app.route('/')
 def homepage():
   return render_template('index.html')
@@ -14,7 +27,28 @@ def homepage():
 @app.route('/inference')
 def inferencepage():
   return render_template('inference.html')
-  
+
+@app.route('/processimage', methods=["GET","POST"])
+def processimage():
+  filecontents = request.json["file"][22:]
+  filecontents = str.encode(filecontents)
+  with open("output.png", "wb") as file:
+    file.write(base64.decodebytes(filecontents))
+
+  image = Image.open("output.png").convert('RGB')
+  image = np.asarray(image)
+  results = model.predict(image)
+  for r in results:
+    boxes = r.boxes
+    for box in boxes:
+      b = box.xyxy[0]# get box coordinates in (top, left, bottom, right) format
+      b = b.tolist()
+      print("Printing the b: "+ str(b))
+      c = box.cls
+      c= c.tolist()
+      
+  return "Success", 200
+
 @app.errorhandler(500)
 def fivehundrederror(error):
   return render_template('error_500.html')
@@ -23,34 +57,5 @@ def fivehundrederror(error):
 def invalid_route(error):
   return render_template('error_404.html')
 
-
-# Albert's code'
-
-@app.route("/detect", methods=["POST"])
-def detect():
-    buf = request.files["image_file"]
-    boxes = detect_objects_on_image(Image.open(buf.stream))
-    return Response(
-      json.dumps(boxes),  
-      mimetype='application/json'
-    )
-
-def detect_objects_on_image(buf):
-    model = YOLO("best.pt")
-    results = model.predict(buf)
-    result = results[0]
-    output = []
-    for box in result.boxes:
-        x1, y1, x2, y2 = [
-          round(x) for x in box.xyxy[0].tolist()
-        ]
-        class_id = box.cls[0].item()
-        prob = round(box.conf[0].item(), 2)
-        output.append([
-          x1, y1, x2, y2, result.names[class_id], prob
-        ])
-    return output
-
-
-#Albet's code
+print("Starting server...")
 app.run(host='0.0.0.0', port=3000, debug=False)
