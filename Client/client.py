6adbf920354d96296d2ad718807eb4e6a89257ef
AW_Dev <riptidegamesceo@gmail.com>
2024-06-08 09:15:36 -0700

    updated stuff

--- Client/client.py
+++ Client/client.py
@@ -1,9 +1,44 @@
 from tkinter import *
 from tkinter import ttk
+import cv2
+from PIL import Image, ImageTk
+import numpy as np
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+imagewidth = 800
+imageheight = 800
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, imagewidth)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, imageheight)
 
 root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+root.attributes('-fullscreen', True)
+root.title = "Recycl.ai Client GUI"
+
+style = ttk.Style()
+style.configure("BW.TLabel", foreground="white", background="#ff00ff")
+
+label = Label(root, text="Image")
+label.pack()
+
+label.place(
+    relx=0.0,
+    rely=0.5,
+    anchor='w')
+
+
+def openCameraFeed():
+    while True:
+        _, img = cap.read()
+        b, g, r = cv2.split(img)
+        img = cv2.merge((r, g, b))
+        img = cv2.resize(img, (800, 600))
+        img = Image.fromarray(img)
+        imtk = ImageTk.PhotoImage(image=img)
+        label.configure(image=imtk)
+        label.image = imtk
+        label.update()
+
+
+root.after(0, openCameraFeed)
 root.mainloop()
--- Server/server.py
+++ Server/server.py
@@ -1,79 +1,87 @@
+from PIL import Image
+import numpy as np
+from ultralytics import YOLO
 from flask import Flask, render_template, request, jsonify
 import base64
 import shutil
 print("Importing AI Packages...")
 print("Importing YOLO...")
-from ultralytics import YOLO
 print("YOLO imported. Importing numpy...")
-import numpy as np
 print("numpy imported. Importing PIL...")
-from PIL import Image
 print("PIL imported.")
 
 print("Loading model...")
-model = YOLO("Server\\best.pt")
+model = YOLO("best.pt")
 print("Model loaded.")
 
-app = Flask(__name__) # Create an Instance
+app = Flask(__name__)  # Create an Instance
+
+
 @app.route('/')
 def homepage():
-  return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/inference')
 def inferencepage():
-  return render_template('inference.html')
+    return render_template('inference.html')
 
-@app.route('/processimage', methods=["GET","POST"])
+
+@app.route('/processimage', methods=["GET", "POST"])
 def processimage():
-  filecontents = request.json["file"][22:]
-  filecontents = str.encode(filecontents)
-  with open("output.png", "wb") as file:
-    file.write(base64.decodebytes(filecontents))
-    
-  image = Image.open("output.png").convert('RGB')
-  image = np.asarray(image)
-  image = image[...,::-1]
-  results = model.predict(image, save=True)
-  for r in results:
-    boxes = r.boxes
-    speed = r.speed['inference']
-    confidencelist = []
-    classlist = []
-    for box in boxes:
-      boxclass = box.cls.tolist()[0]
-      classlist.append(boxclass)
-      confidencelist.append(box.conf.tolist()[0])
-  print(confidencelist)
-  confidencelist.sort(reverse=True)
-  print(confidencelist)
-  try:
-    didfail = classlist[0] is None
-  except Exception as e:
-    didfail = True
-  print(didfail)
-  jsonresults = {
-    "nameid" : int(classlist[0]) if not didfail else 0,
-    "confidence" : confidencelist[0] if not didfail else 0,
-    "speed" : speed,
-    "failed" : didfail
-  }
-  shutil.move("runs\detect\predict\image0.jpg", "Server/static/output.png")
-  shutil.rmtree("runs")
-  print(jsonresults)
-  return jsonify(jsonresults)
+    filecontents = request.json["file"][22:]
+    filecontents = str.encode(filecontents)
+    with open("output.png", "wb") as file:
+        file.write(base64.decodebytes(filecontents))
+
+    image = Image.open("output.png").convert('RGB')
+    image = np.asarray(image)
+    image = image[..., ::-1]
+    results = model.predict(image, save=True)
+    for r in results:
+        boxes = r.boxes
+        speed = r.speed['inference']
+        confidencelist = []
+        classlist = []
+        for box in boxes:
+            boxclass = box.cls.tolist()[0]
+            classlist.append(boxclass)
+            confidencelist.append(box.conf.tolist()[0])
+    print(confidencelist)
+    confidencelist.sort(reverse=True)
+    print(confidencelist)
+    try:
+        didfail = classlist[0] is None
+    except Exception as e:
+        didfail = True
+    print(didfail)
+    jsonresults = {
+        "nameid": int(classlist[0]) if not didfail else 0,
+        "confidence": confidencelist[0] if not didfail else 0,
+        "speed": speed,
+        "failed": didfail
+    }
+    shutil.move("runs\detect\predict2\image0.jpg", "static/output.png")
+    shutil.rmtree("runs")
+    print(jsonresults)
+    return jsonify(jsonresults)
+
 
 @app.route("/500")
 def fakefivehundo():
-  print(1 + "asdasd")
-  return "how in the hell does this still work"
+    print(1 + "asdasd")
+    return "how in the hell does this still work"
+
 
 @app.errorhandler(500)
 def fivehundrederror(error):
-  return render_template('error_500.html')
-  
+    return render_template('error_500.html')
+
+
 @app.errorhandler(404)
 def invalid_route(error):
-  return render_template('error_404.html')
+    return render_template('error_404.html')
+
 
 print("Starting server...")
 app.run(host='0.0.0.0', port=3000, debug=False)
